bc8cb1fa374ddba797f2c5c42faa5543db5e3e41
ChoiJIN <zuharu@gmail.com>
2016-05-09 01:39:03 -0700

    initial project

---
+++ photo/admin.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+from django.contrib import admin
+
+from photo.models import Photo
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'image_file', 'filtered_image_file']
+
+admin.site.register(Photo, PhotoAdmin)
